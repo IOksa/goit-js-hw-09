a70aff71bc377e3e0ad3abd0d7a553b432268d86
IOksa <117656767+IOksa@users.noreply.github.com>
2023-04-13 22:18:19 +0300

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,32 @@
 const refs={
-    buttonStart: document.querySelector('data-start'),
-    buttonStop: document.querySelector('data-stop'),
+    buttonStart: document.querySelector('button[data-start]'),
+    buttonStop: document.querySelector('button[data-stop]'),
+    bodyEl: document.querySelector('body'),
+};
+
+let intervalId = null;
+const DELAY=1000;
+
+refs.buttonStop.classList.add('disabled');
+refs.buttonStart.addEventListener('click', onClickBtnStart);
+refs.buttonStop.addEventListener('click', onClickBtnStop);
+
+
+function onClickBtnStart(){
+    refs.buttonStart.classList.add('disabled');
+    refs.buttonStop.classList.remove('disabled');
+    intervalId=setInterval(() => {
+        refs.bodyEl.style.backgroundColor=getRandomHexColor();  
+    }, DELAY);
+   
 }
 
-console.log(buttonStart);
-console.log(buttonStop);
+function onClickBtnStop(){
+    refs.buttonStart.classList.remove('disabled');
+    refs.buttonStop.classList.add('disabled');
+    clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
---
+++ src/js/02-timer.js
@@ -0,0 +1,92 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs={
+    butStart: document.querySelector('button[data-start]'),
+    spanDays: document.querySelector('span[data-days]'),
+    spanHours: document.querySelector('span[data-hours]'),
+    spanMinutes: document.querySelector('span[data-minutes]'),
+    spanSeconds: document.querySelector('span[data-seconds]'),
+};
+
+let selectedTime=null;
+let intervalId = null;
+const DELAY=1000;
+
+refs.butStart.classList.add('disabled');
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+
+    onClose(selectedDates) {
+    
+      const currentTime = Date.now();
+      if((selectedDates[0]-currentTime)>0){
+         refs.butStart.classList.remove('disabled');
+         selectedTime=selectedDates[0];
+        }
+        else{
+            Notify.failure('Please choose a date in the future');
+           
+        }
+
+    },
+};
+
+flatpickr("#datetime-picker", options);
+
+refs.butStart.addEventListener('click', onClickBtnStart);
+
+function onClickBtnStart(){
+    refs.butStart.classList.add('disabled');
+
+    intervalId=setInterval(() => {   
+        const currentTime = Date.now();
+        const convertData=convertMs(selectedTime-currentTime);
+
+        drawTimer(convertData);
+
+        if((convertData.days===0 && convertData.hours===0 && convertData.minutes===0 && convertData.seconds===0)){
+            clearInterval(intervalId);
+           
+        }
+    }, DELAY);
+   
+}
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+}
+
+function drawTimer({days, hours, minutes, seconds}){
+    refs.spanDays.textContent=addLeadingZero(days);
+    refs.spanHours.textContent=addLeadingZero(hours);
+    refs.spanMinutes.textContent=addLeadingZero(minutes);
+    refs.spanSeconds.textContent=addLeadingZero(seconds);
+}
+
+function addLeadingZero(value){
+    return String(value).padStart(2, '0');
+}
+  
