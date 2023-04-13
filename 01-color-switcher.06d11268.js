9d92b168bd6811318455123e690cc9280aeb1b46
IOksa <117656767+IOksa@users.noreply.github.com>
2023-04-13 12:31:25 +0000

    Deploying to gh-pages from @ IOksa/goit-js-hw-09@13ad61e219d743b95799ca851684a21e58224e03 🚀

---
+++ 01-color-switcher.06d11268.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector("data-start"),document.querySelector("data-stop");console.log(buttonStart),console.log(buttonStop)}();
+//# sourceMappingURL=01-color-switcher.06d11268.js.map
---
+++ 01-color-switcher.8e3de685.js
@@ -0,0 +1,2 @@
+document.querySelector("data-start"),document.querySelector("data-stop");console.log(buttonStart),console.log(buttonStop);
+//# sourceMappingURL=01-color-switcher.8e3de685.js.map
